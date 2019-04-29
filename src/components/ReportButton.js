edb3364d04dccec5538e6fa8f4265c08b3347973
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2019-04-28 19:37:02 -0700

    Template app header

---
+++ src/components/ReportButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../assets/styles/ReportButton.css';
+
+class ReportButton extends Component {
+    state = {  }
+    render() {
+        return ( <div>
+            <button>Report</button>
+        </div> );
+    }
+}
+
+export default ReportButton;
