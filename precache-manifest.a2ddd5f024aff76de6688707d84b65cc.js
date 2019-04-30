d3c03a7b4e2ce0b4112ae867cb33a6e9d8174e91
JeongUkJae <JeongUkJae@gmail.com>
2019-04-30 03:46:02 -0700

    Updates

---
+++ precache-manifest.a2ddd5f024aff76de6688707d84b65cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83aaddfa16c7266ac2d30091ef269d2",
+    "url": "/citywatch-frontend/index.html"
+  },
+  {
+    "revision": "1cdcd8e654f317edb3b7",
+    "url": "/citywatch-frontend/static/css/main.64c9386d.chunk.css"
+  },
+  {
+    "revision": "36d2ba60de52ae8997b8",
+    "url": "/citywatch-frontend/static/js/2.860ef78e.chunk.js"
+  },
+  {
+    "revision": "1cdcd8e654f317edb3b7",
+    "url": "/citywatch-frontend/static/js/main.349a7f32.chunk.js"
+  },
+  {
+    "revision": "0d40cf7a04394df4e42d",
+    "url": "/citywatch-frontend/static/js/runtime~main.bb92eaf1.js"
+  }
+]);
